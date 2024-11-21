a9c713ebdead0e5c0e43e238b4d782c137cd689d
KUSHANK PATIL <patilkushank@gmail.com>
2024-11-21 20:12:10 +0530

    2nd Same Commit

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -8,6 +8,8 @@ const router = express.Router();
 
 router.post('/signup', AuthRequestValidators.validateUserAuth,UserController.create);
 router.post('/signIn',AuthRequestValidators.validateUserAuth,UserController.signIn);
+router.get( '/isAuthenticated',UserController.isAuthenticated);
+
 
 
 
